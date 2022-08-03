29dcb162eb177bb7525ede17b360d31d5924cca1
nanfengzhiwo <110539252+nanfengzhiwo@users.noreply.github.com>
2022-08-03 22:00:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def index():
         r1=model1.predict([[rates]])
         model2=joblib.load("tree")
         r2=model2.predict([[rates]])
-        return(render_template("index.html", result="OK",result2="OK"))
+        return(render_template("index.html", result1=r1,result2=r2))
     else:
         return(render_template("index.html", result1="Waiting",result2="Waiting"))
 
